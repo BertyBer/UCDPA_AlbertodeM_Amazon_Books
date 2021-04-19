d9933ebf8f596b0daf08f523d46dc5cdc419fa93
BertyBer <chiskyxl@hotmail.com>
2021-04-20 00:31:17 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Initial coding
+
+#print("hello what is your name")
+#myName = input()
+#print("Really good to see you here, " + myName)
+#print("Fun fact, the length of your name is:")
+#print(len(myName))
+#print("what is your age?")
+#myAge = input()
+#print ("Do you know you will be " + str(int(myAge)+6)+ " in six years time?")
+#print("I know, mind blowing!! " + myName)
+#print("OK, enough talking, let's do this")
